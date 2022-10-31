d52413b74d6dfb8c1e9263418b3cab06d504c89b
rohit <rohitpushp123@gmail.com>
2022-10-31 22:22:57 +0530

    Completed

--- src/main/java/accountopeningservice/accountopeningservice/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package accountopeningservice.accountopeningservice.repository;
-
-import accountopeningservice.accountopeningservice.model.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account,Long> {
-}
